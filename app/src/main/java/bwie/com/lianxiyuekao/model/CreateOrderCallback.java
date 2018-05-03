634c62169f5b4a912278167cea1c534d45109256
1275021828@QQ.COM <12750218282@QQ.com>
2018-05-03 20:29:31 +0800

    Initial commit

---
+++ app/src/main/java/bwie/com/lianxiyuekao/model/CreateOrderCallback.java
@@ -0,0 +1,14 @@
+package bwie.com.lianxiyuekao.model;
+
+import bwie.com.lianxiyuekao.bean.CreatOrderBean;
+
+/**
+ * Created by BAIPEI on 2018/1/17.
+ */
+
+public interface CreateOrderCallback {
+
+    void success(CreatOrderBean bean);
+    void failure(int code);
+
+}
